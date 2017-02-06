9f713c915a8aebe277c4e21fd473162c341fc629
Nick Small <nick@dds.mil>
2017-02-05 22:49:59 -0500

    Experiment shoving entire MCM HTML into page

--- js/main.js
+++ js/main.js
@@ -1,28 +1,51 @@
-(function() {
+$(function() {
+	var navSelector = "#toc"
+	var $nav = $(navSelector)
+
+	$('body').scrollspy({
+		target: navSelector
+	})
+
 	var navItem = document.querySelector('[data-navitem-proto]')
+	var contentItem = document.querySelector('[data-content-proto]')
+
 	var container = navItem.parentNode
 	container.removeChild(navItem)
+	container.removeChild(contentItem)
 
-	fetch('/toc.json', function(json) {
-		var toc = JSON.parse(json)
-		for (var key in toc) {
-			var item = navItem.cloneNode(true)
-			var comps = key.split(' - ')
-			if (comps.length > 1) {
-				var romanized = [romanize(comps[0])]
-				if (!romanized[0]) romanized.pop()
-				romanized.push(comps[1])
-				romanized = romanized.join(' - ')
-			} else { romanized = comps[0] }
-			item.querySelector('h2').innerHTML = romanized
-			container.appendChild(item)
+	// fetch('/toc.json', function(json) {
+	// 	var toc = JSON.parse(json)
+	// 	for (var key in toc) {
+	// 		var item = navItem.cloneNode(true)
+	// 		var comps = key.split(' - ')
+	// 		if (comps.length > 1) {
+	// 			var romanized = [romanize(comps[0])]
+	// 			if (!romanized[0]) romanized.pop()
+	// 			romanized.push(comps[1])
+	// 			romanized = romanized.join(' - ')
+	// 		} else { romanized = comps[0] }
+	// 		item.querySelector('h2').innerHTML = romanized
+	// 		container.appendChild(item)
+	// 	}
+	// })
+
+	var REPLACEMENTS = {
+		header: 'h1',
+		list: 'ol'
+	}
+
+	fetch('/mcm.html', function(html) {
+		console.log('loaded')
+		for (var key in REPLACEMENTS) {
+			html = html.replace(new RegExp("<(/?)" + key, 'g'), "<$1" + REPLACEMENTS[key])
 		}
-	})
 
-	fetch('/mcm.html', function() {
+console.log('replaced')
+		document.getElementById('content').innerHTML = html
+		console.log('rendered')
 
+		Toc.init($nav)
 	})
-	
 
 	function fetch(url, callback) {
 		var request = new XMLHttpRequest()
@@ -47,4 +70,4 @@
 
 	}
 
-})()
+})
