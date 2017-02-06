9222ec37830666fcf4df3c91b79955ea35c081bb
Nick Small <nick@dds.mil>
2017-02-06 00:21:40 -0500

    Try out JSON content approach

--- js/main.js
+++ js/main.js
@@ -1,57 +1,99 @@
 $(function() {
-	var navSelector = "#toc"
-	var $nav = $(navSelector)
-
-	$('body').scrollspy({
-		target: navSelector
-	})
-
-	var navItem = document.querySelector('[data-navitem-proto]')
-	var contentItem = document.querySelector('[data-content-proto]')
-
-	var container = navItem.parentNode
-	container.removeChild(navItem)
-	container.removeChild(contentItem)
-
-	// fetch('/toc.json', function(json) {
-	// 	var toc = JSON.parse(json)
-	// 	for (var key in toc) {
-	// 		var item = navItem.cloneNode(true)
-	// 		var comps = key.split(' - ')
-	// 		if (comps.length > 1) {
-	// 			var romanized = [romanize(comps[0])]
-	// 			if (!romanized[0]) romanized.pop()
-	// 			romanized.push(comps[1])
-	// 			romanized = romanized.join(' - ')
-	// 		} else { romanized = comps[0] }
-	// 		item.querySelector('h2').innerHTML = romanized
-	// 		container.appendChild(item)
-	// 	}
+	// $('body').scrollspy({
+	// 	target: navSelector
 	// })
 
-	var REPLACEMENTS = {
-		header: 'h1',
-		list: 'ol'
+	var navItemProto = document.querySelector('[data-nav-proto]')
+	var content = document.querySelector('[data-content]')
+
+	var navContainer = navItemProto.parentNode
+	navContainer.removeChild(navItemProto)
+
+	fetch('/toc.json', function(json) {
+		window.MCM = JSON.parse(json)
+
+		renderNavItems(navContainer, MCM, true)
+
+		$('body').scrollspy({
+			target: '[data-toc]'
+		})
+	})
+
+	function renderPart(key, html) {
+		var result = "<h1 style='text-align: center'>"
+		var id = parseInt(key)
+		if (id) result += "Part " + romanize(id) + "<br>"
+		result += key.split(' - ')[1].toUpperCase()
+		result += "</h1>"
+		result += contentize(html)
+		return result
 	}
 
-	fetch('/mcm.html', function(html) {
-		console.log('loaded')
-		for (var key in REPLACEMENTS) {
-			html = html.replace(new RegExp("<(/?)" + key, 'g'), "<$1" + REPLACEMENTS[key])
+	function renderNavItems(container, sections, renderChildren) {
+		container.innerHTML = ''
+
+		for (var key in sections) {
+			var value = sections[key]
+			var navItem = renderNavItem(key, renderChildren && typeof value !== 'string' && value)
+			container.appendChild(navItem)
+
+			if (renderChildren && typeof value === 'string') {
+				var wrapper = document.createElement('div')
+				wrapper.id = parameterize(key)
+				wrapper.innerHTML = renderPart(key, value)
+				content.appendChild(wrapper)
+			}
+		}
+	}
+
+	function renderNavItem(title, children) {
+		var item = navItemProto.cloneNode(true)
+		item.querySelector('[data-title]').innerHTML = romanizeTitle(title)
+		item.querySelector('[data-target]').href = '#' + parameterize(title)
+
+		var subNav = item.querySelector('[data-subnav]')
+		if (children) {
+			renderNavItems(subNav, children)
+		} else {
+			item.removeChild(subNav)
 		}
 
-console.log('replaced')
-		document.getElementById('content').innerHTML = html
-		console.log('rendered')
-
-		Toc.init($nav)
-	})
+		return item
+	}
 
 	function fetch(url, callback) {
 		var request = new XMLHttpRequest()
 		request.addEventListener('load', function() {callback(this.responseText)})
 		request.open('GET', url)
 		request.send()
+	}
+
+	function parameterize(string) {
+		return string.trim().replace(/[\s|-]+/g, '-').split(/[^\w|-]/)[0].toLowerCase()
+	}
+
+	var REPLACEMENTS = {
+		header: 'h3',
+		list: 'ol'
+	}
+
+	function contentize(html) {
+		for (var key in REPLACEMENTS) {
+			html = html.replace(new RegExp("<(/?)" + key, 'g'), "<$1" + REPLACEMENTS[key])
+		}
+
+		return html
+	}
+
+	function romanizeTitle(title) {
+		var comps = title.split(' - ')
+		if (comps.length > 1) {
+			var romanized = [romanize(comps[0])]
+			if (!romanized[0]) romanized.pop()
+				
+			romanized.push(comps[1])
+			return romanized.join(' - ')
+		} else { return comps[0] }
 	}
 
 	function romanize(num) {
@@ -65,9 +107,4 @@ console.log('replaced')
 		
 		return roman
 	}
-
-	function pushPage(path) {
-
-	}
-
 })
